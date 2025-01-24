4447ce0bd274ae9d2601417c14ffb7b4a70e7c48
Jeet <jeetnath2110@gmail.com>
2025-01-24 23:40:35 +0530

    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,8 @@
 @extends('layouts.home')
 @section('tittle', 'Home')
 
-@section('css-cdn')
-
-{{--bootstarp css cdn--}}
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-{{--fontawesome cdn--}}
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-{{--google fonts cdn--}}
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
-
-{{--google fonts cdn 2--}}
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-{{--universal css cdn--}}
-<link rel="stylesheet" href="{{ asset('essentials/css/unv.css') }}">
+@section('pages-css')
 
 {{--custom page css cdn--}}
 <link rel="stylesheet" href="{{ asset('essentials/css/home-page.css') }}">
-
 @endsection
