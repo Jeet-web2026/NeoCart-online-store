7e8b782c40787ad66adb0202cf44a58ceb5e192b
Jeet <jeetnath2110@gmail.com>
2025-01-24 23:37:30 +0530

    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,28 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace( '_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Csrf-token" content="{{ csrf_token() }}">
-    <title>@yield ('tittle') | {{ config('app.name') }}</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.home')
+@section('tittle', 'Home')
+
+@section('css-cdn')
+
+{{--bootstarp css cdn--}}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+{{--fontawesome cdn--}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+{{--google fonts cdn--}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
+{{--google fonts cdn 2--}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+{{--universal css cdn--}}
+<link rel="stylesheet" href="{{ asset('essentials/css/unv.css') }}">
+
+{{--custom page css cdn--}}
+<link rel="stylesheet" href="{{ asset('essentials/css/home-page.css') }}">
+
+@endsection
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace( '_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Csrf-token" content="{{ csrf_token() }}">
+    <title>@yield ('tittle') | {{ config('app.name') }}</title>
+
+    @yield('css-cdn')
+    @yield('universal-css')
+    @yield('pages-css')
+</head>
+
+<body>
+
+</body>
+
+</html>
