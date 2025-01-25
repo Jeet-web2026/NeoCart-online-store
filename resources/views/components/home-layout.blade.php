6e7475369f6e3486ac8a8235f994270d25b00a79
Jeet <jeetnath2110@gmail.com>
2025-01-26 00:40:23 +0530

    j

--- resources/views/components/home-layout.blade.php
+++ resources/views/components/home-layout.blade.php
@@ -1,22 +1,4 @@
-<x-main-layout tittle="Home" bodyId="main-content">
-    @section('page-css')
-    <style>
-        #main-content {
-            height: 100vh;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            padding-top: 30vh;
-        }
-
-        #main-content .home-main-content .card-img {
-            height: 50vh;
-            object-fit: cover;
-            width: 100%;
-        }
-    </style>
-    @endsection
+<x-main-layout tittle="Home" bodyId ="main-content">
     @include('components.navbar')
     {{ $slot }}
 </x-main-layout>
