9157bbcd2b048a012383e721cce35bf88e9cff63
Jeet <jeetnath2110@gmail.com>
2025-01-26 01:01:44 +0530

    j

--- resources/views/components/home-layout.blade.php
+++ resources/views/components/home-layout.blade.php
@@ -1,4 +1,8 @@
-<x-main-layout tittle="Home" bodyId ="main-content">
+<x-main-layout tittle="Home" bodyId="main-content">
+    @section('page-css')
+    <link rel="stylesheet" href="{{ asset('essentials/css/home-style.css') }}">
+    @endsection
     @include('components.navbar')
     {{ $slot }}
+    @include('components.home-popups')
 </x-main-layout>
---
+++ resources/views/components/home-popups.blade.php
@@ -0,0 +1,25 @@
+{{--wishlist--}}
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="wishlist" aria-labelledby="wishlistLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="wishlistLabel">Backdrop with scrolling</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <p>Try scrolling the rest of the page to see this option in action.</p>
+    </div>
+</div>
+{{--wishlist--}}
+
+{{--cart--}}
+<div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="viewCart" aria-labelledby="viewCartLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="viewCartLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div>
+            I will not close if you click outside of me.
+        </div>
+    </div>
+</div>
+{{--cart--}}
