113c71a6992aff9125df2ee6513dda5f7669952c
Jeet <jeetnath2110@gmail.com>
2025-01-27 22:24:33 +0530

    j

--- resources/views/components/home-popups.blade.php
+++ resources/views/components/home-popups.blade.php
@@ -2,7 +2,7 @@
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="wishlist" aria-labelledby="wishlistLabel">
     <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="wishlistLabel">Backdrop with scrolling</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button type="button" class="btn-close border-0 shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
         <p>Try scrolling the rest of the page to see this option in action.</p>
@@ -14,7 +14,7 @@
 <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="viewCart" aria-labelledby="viewCartLabel">
     <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="viewCartLabel">Offcanvas</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button type="button" class="btn-close border-0 shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
         <div>
