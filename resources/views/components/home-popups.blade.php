d37fb5c1a6b68b056009befbad0871aa2765a1d1
Jeet <jeetnath2110@gmail.com>
2025-01-27 23:52:55 +0530

    j

--- resources/views/components/home-popups.blade.php
+++ resources/views/components/home-popups.blade.php
@@ -13,7 +13,7 @@
 {{--cart--}}
 <div class="offcanvas offcanvas-start bg-warning" data-bs-backdrop="static" tabindex="-1" id="viewCart" aria-labelledby="viewCartLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="viewCartLabel">Offcanvas</h5>
+        <h5 class="offcanvas-title text-black" id="viewCartLabel">My orders</h5>
         <button type="button" class="btn-close border-0 shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body d-flex justify-content-center align-items-center">
