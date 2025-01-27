ee63b726ea7e846d008522f9bed3d18a3b649e58
Jeet <jeetnath2110@gmail.com>
2025-01-27 22:42:36 +0530

    j

--- resources/views/components/home-popups.blade.php
+++ resources/views/components/home-popups.blade.php
@@ -1,11 +1,11 @@
 {{--wishlist--}}
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="wishlist" aria-labelledby="wishlistLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="wishlistLabel">Backdrop with scrolling</h5>
+        <h5 class="offcanvas-title text-center" id="wishlistLabel">Backdrop with scrolling</h5>
         <button type="button" class="btn-close border-0 shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-    <div class="offcanvas-body">
-        <p>Try scrolling the rest of the page to see this option in action.</p>
+    <div class="offcanvas-body d-flex justify-content-center align-items-center">
+        <x-loginForm />
     </div>
 </div>
 {{--wishlist--}}
@@ -16,10 +16,8 @@
         <h5 class="offcanvas-title" id="viewCartLabel">Offcanvas</h5>
         <button type="button" class="btn-close border-0 shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-    <div class="offcanvas-body">
-        <div>
-            I will not close if you click outside of me.
-        </div>
+    <div class="offcanvas-body d-flex justify-content-center align-items-center">
+        <x-loginForm />
     </div>
 </div>
 {{--cart--}}
---
+++ resources/views/components/loginForm.blade.php
@@ -0,0 +1,12 @@
+<form class="border p-3 rounded-3">
+    <div class="mb-3">
+        <label for="user-email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="user-email" name="user-email">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="user-password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="user-password">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
