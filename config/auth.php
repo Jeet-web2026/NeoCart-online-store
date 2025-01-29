8837ee486e3336be55ce78c6eba7bdae81e19ed4
Jeet <jeetnath2110@gmail.com>
2025-01-30 01:08:22 +0530

    j

--- config/auth.php
+++ config/auth.php
@@ -2,34 +2,37 @@
 
 return [
 
-
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
+
     'guards' => [
-        'admin' => [
+        'web' => [
             'driver' => 'session',
-            'provider' => 'admin_logins',
+            'provider' => 'users',
         ],
     ],
 
+
     'providers' => [
-        'admins' => [
+        'users' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
     ],
 
+
     'passwords' => [
-        'admins' => [
-            'provider' => 'admins',
+        'users' => [
+            'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
         ],
     ],
+
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
