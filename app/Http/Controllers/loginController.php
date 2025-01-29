8cd1afe50b272a06a0f1a831c166f88cc0ca1c60
Jeet <jeetnath2110@gmail.com>
2025-01-30 01:59:48 +0530

    j

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class loginController extends Controller
 {
-    public function wishlistRegister()
+    public function wishlistRegister(Request $request)
     {
+
+        $vallidatedata = $request->validate([
+            'user-name' => 'required|min:2',
+            'user-email' => 'required|email',
+            'user-pincode' => 'required|min:6',
+            'user-password' => 'required|min:5'
+
+        ]);
+
+        User::create([
+            'name' => $vallidatedata['user-name'],
+            'email' => $vallidatedata['user-email'],
+            'password' => $vallidatedata['user-password'],
+            'pincode' => $vallidatedata['user-pincode'],
+        ]);
+
         return "Register Successfully!";
     }
 }
