ffc96e175d0a1b8fa7901f85f5c1a80d2d73ee31
Jeet <jeetnath2110@gmail.com>
2025-02-02 20:34:52 +0530

    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -127,23 +127,4 @@
     </section>
     {{--sort by categories--}}
 
-    @if(session('logout-message-body'))
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="check-circle-fill" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-        </symbol>
-    </svg>
-    <div class="alert alert-success p-2 d-flex align-items-center rounded-start-5 rounded-end-0" role="alert" id="alert-popups">
-        <svg class="bi flex-shrink-0 me-2" role="img" id="check-fill-img" aria-label="Success:">
-            <use xlink:href="#check-circle-fill" />
-        </svg>
-        <div>
-            @if(session('logout-message-body'))
-            {{ session('logout-message-body') }}
-            @endif
-        </div>
-        <button type="button" class="btn-close notification-btn d-none" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    @endif
-
 </x-home-layout>
