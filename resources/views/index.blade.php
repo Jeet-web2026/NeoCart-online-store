639a13f5a185a9f484f9a60bfc77a7b73c694ea7
Jeet <jeetnath2110@gmail.com>
2025-02-02 15:33:14 +0530

    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,7 +35,7 @@
 
     {{--sort by categories--}}
     <section class="category-wise-sort">
-        <div class="container-fluid p-5">
+        <div class="container-fluid p-5 pt-3">
             <h3 class="text-capitalize text-black fs-5 mb-4 font-500 ps-2">shop by categories</h3>
             <div class="row category">
                 <div class="col p-2">
@@ -133,7 +133,7 @@
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
         </symbol>
     </svg>
-    <div class="alert alert-success d-flex align-items-center rounded-start-5 rounded-end-0" role="alert" id="alert-popups">
+    <div class="alert alert-success p-2 d-flex align-items-center rounded-start-5 rounded-end-0" role="alert" id="alert-popups">
         <svg class="bi flex-shrink-0 me-2" role="img" id="check-fill-img" aria-label="Success:">
             <use xlink:href="#check-circle-fill" />
         </svg>
