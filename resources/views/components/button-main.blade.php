37f55d585be1977a5ffbbb6f6b2eb882d3d45d2d
Jeet <jeetnath2110@gmail.com>
2025-02-06 23:41:30 +0530

    j

--- resources/views/components/button-main.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['btnDes' => '', 'href' => '', 'type' => '', 'style' => '', 'extraclass' => '', 'icon' => ''])
-<a @if($href)href="{{ $href }}" @endif @if($type)type="{{ $type }}" @endif @if($style)style="{{ $style }}" @endif class="bg-warning btn border-0 text-light @if($extraclass) {{ $extraclass }}@endif">
-    @if (!empty($btnDes))
-    {{ $btnDes }}
-    @elseif (!empty($icon))
-    {!! $icon !!}
-    @endif
-</a>
--- resources/views/productmanagement/orderSummery.blade.php
+++
@@ -1,5 +0,0 @@
-<x-main-layout tittle="Order's summery" bodyId="login-content">
-    <x-navbar-layout />
-    <h1>hello</h1>
-    <x-popup-layout />
-</x-main-layout>
