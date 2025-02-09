21bc9fc5dcc04b22385fac0e44408c9a300202af
Jeet <jeetnath2110@gmail.com>
2025-02-09 15:33:34 +0530

    j

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,11 +2,48 @@
     <div class="container-fluid p-3">
         <div class="row">
             <div class="col-md-3">
-
+                <div class="card border-0 bg-transparent">
+                    <div class="card-body">
+                        <h5 class="text-black fze-2 text-capitalize">get know us</h5>
+                        <ul class="ps-0 mt-3">
+                            <li class="fze-1 text-capitalize mb-1"><a href="javascript:void(0)" class="text-black">careers</a></li>
+                            <li class="fze-1 text-capitalize mb-1"><a href="javascript:void(0)" class="text-black">blog</a></li>
+                            <li class="fze-1 text-capitalize mb-1"><a href="javascript:void(0)" class="text-black">about neoCart</a></li>
+                            <li class="fze-1 text-capitalize mb-1"><a href="javascript:void(0)" class="text-black">industry relationships</a></li>
+                            <li class="fze-1 text-capitalize mb-1"><a href="javascript:void(0)" class="text-black">neoCart exclusive</a></li>
+                            <li class="fze-1 text-capitalize"><a href="javascript:void(0)" class="text-black">neoCart explore</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
+            <div class="col-md-3">
+                <div class="card border-0 bg-transparent">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-0 bg-transparent">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-0 bg-transparent">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="card border-0 bg-transparent">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
