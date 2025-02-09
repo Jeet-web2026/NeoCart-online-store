5f3578e7e4f733df7418fa8759c4335e931c4338
Jeet <jeetnath2110@gmail.com>
2025-02-09 13:00:16 +0530

    j

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,39 +1,69 @@
 <section class="shadow fixed-top">
     <section id="top-nav">
         <div class="container-fluid py-2">
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col-md-6">
-                    <a href="javascript:void(0)" class="text-secondary">Latest news: Covid 19</a>
+                    <a href="javascript:void(0)" class="text-secondary fze-1">Latest news: Covid 19</a>
                 </div>
-                <div class="col-md-6"></div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col">
+                            <div class="dropdown">
+                                <button class="btn shadow-none border-0 fze-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fa-solid fa-globe me-2"></i>Country<i class="fa-solid fa-chevron-down ms-2"></i>
+                                </button>
+                                <ul class="dropdown-menu no-radius border-0 shadow">
+                                    <li><a class="dropdown-item fze-1" href="javascript:void(0)">Action</a></li>
+                                    <li><a class="dropdown-item fze-1" href="javascript:void(0)">Another action</a></li>
+                                    <li><a class="dropdown-item fze-1" href="javascript:void(0)">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="dropdown">
+                                <button class="btn shadow-none border-0 fze-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fa-solid fa-globe me-2"></i>Country<i class="fa-solid fa-chevron-down ms-2"></i>
+                                </button>
+                                <ul class="dropdown-menu no-radius border-0 shadow">
+                                    <li><a class="dropdown-item fze-1" href="javascript:void(0)">Action</a></li>
+                                    <li><a class="dropdown-item fze-1" href="javascript:void(0)">Another action</a></li>
+                                    <li><a class="dropdown-item fze-1" href="javascript:void(0)">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col"></div>
+                        <div class="col"></div>
+                        <div class="col"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid pb-2">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="javascript:void(0)">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="javascript:void(0)navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link text-black" href="#">Home</a>
+                        <a class="nav-link text-black" href="javascript:void(0)">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="javascript:void(0)">Link</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="javascript:void(0)" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="javascript:void(0)">Action</a></li>
+                            <li><a class="dropdown-item" href="javascript:void(0)">Another action</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="javascript:void(0)">Something else here</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
@@ -42,22 +72,22 @@
                 </ul>
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="javascript:void(0)">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="javascript:void(0)">Link</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="javascript:void(0)" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="javascript:void(0)">Action</a></li>
+                            <li><a class="dropdown-item" href="javascript:void(0)">Another action</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="javascript:void(0)">Something else here</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
