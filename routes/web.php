58ba99507ddb1196a1a4a91fe35ddff324a5dc47
Jeet <jeetnath2110@gmail.com>
2025-02-09 11:24:53 +0530

    j

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
 
 // home routes
-Route::get('/', [homeController::class, 'index'])->name('login');
+Route::get('/', [homeController::class, 'index'])->name('home');
