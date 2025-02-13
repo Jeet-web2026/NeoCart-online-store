aa3900d4cea513f06ccc1b95380b1477becf405e
Jeet <jeetnath2110@gmail.com>
2025-02-14 01:42:29 +0530

    j

--- resources/views/login-page.blade.php
+++ resources/views/login-page.blade.php
@@ -14,12 +14,11 @@
     {{--login form start--}}
     <div class="container-fluid d-flex justify-content-center align-items-center">
         <div class="border shadow w-50 p-4 rounded-3">
-            <h3 class="text-center fze-4 fw-bold text-black text-uppercase">login</h3>
+            <h3 class="text-center fze-4 fw-bold text-black text-uppercase my-3">login</h3>
             <form>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <label for="user-email-address" class="form-label fze-2 font-500">Email address</label>
+                    <input type="email" class="form-control" id="user-email-address" name="user-email-address">
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -27,6 +26,7 @@
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+            <p class="text-center text-capitalize fze-1 mt-3">already have an account? <a href="">Login now</a></p>
         </div>
     </div>
     {{--login form end--}}
