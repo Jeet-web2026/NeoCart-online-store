2ac425dce0de08373d80a5c5c336f3649a44782b
Jeet <jeetnath2110@gmail.com>
2025-02-14 01:37:35 +0530

    j

--- resources/views/login-page.blade.php
+++ resources/views/login-page.blade.php
@@ -1,3 +1,35 @@
-<x-DefaultLayout title="Signup">
+<x-DefaultLayout title="Login" bodyId="login">
+    @section('extracss')
+    <style>
+        #login {
+            height: 100vh;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+    @endsection
+
+    {{--login form start--}}
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+        <div class="border shadow w-50 p-4 rounded-3">
+            <h3 class="text-center fze-4 fw-bold text-black text-uppercase">login</h3>
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+    {{--login form end--}}
+
 
 </x-DefaultLayout>
