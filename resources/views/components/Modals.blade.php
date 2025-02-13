36683c0a25a94622a988f953122b123343687329
Jeet <jeetnath2110@gmail.com>
2025-02-14 00:45:28 +0530

    j

--- resources/views/components/Modals.blade.php
+++ resources/views/components/Modals.blade.php
@@ -1,16 +1,19 @@
 <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="searchModalLabel">Modal title</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h1 class="modal-title fs-5" id="searchModalLabel">Search box</h1>
+                <button type="button" class="btn-close border-0 shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">@Search</span>
+                    <input type="text" class="form-control" placeholder="Search your query" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-outline-secondary shadow-none" data-bs-dismiss="modal"><i class="fa-solid fa-xmark"></i></button>
+                <button type="button" class="btn btn-outline-success shadow-none">Search</button>
             </div>
         </div>
     </div>
