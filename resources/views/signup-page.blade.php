bc211b105fbe98aa9fc08acb03618af72cc33c8a
Jeet <jeetnath2110@gmail.com>
2025-02-16 13:46:39 +0530

    j

--- resources/views/signup-page.blade.php
+++ resources/views/signup-page.blade.php
@@ -20,18 +20,30 @@
                 <div class="mb-3">
                     <label for="user-full-name" class="form-label fze-2 font-500 text-black">Full name</label>
                     <input type="text" class="form-control shadow-none" id="user-full-name" name="user-full-name">
+                    @error('user-full-name')
+                    <p class="text-danger fst-italic fze">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="user-email-address" class="form-label fze-2 font-500 text-black">Email address</label>
                     <input type="email" class="form-control shadow-none" id="user-email-address" name="user-email-address">
+                    @error('user-email-address')
+                    <p class="text-danger fst-italic fze">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="user-password" class="form-label fze-2 font-500 text-black">Password</label>
                     <input type="password" class="form-control shadow-none" id="user-password" name="user-password">
+                    @error('user-password')
+                    <p class="text-danger fst-italic fze">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="user-address" class="form-label fze-2 font-500 text-black">Address</label>
                     <input type="text" class="form-control shadow-none" id="user-address" name="user-address">
+                    @error('user-address')
+                    <p class="text-danger fst-italic fze">{{ $message }}</p>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary border-0 shadow-none">Submit</button>
                 <a class="btn btn-primary border-0 shadow-none" href="{{ url('/') }}">Back</a>
