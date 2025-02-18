0e1d50c8b1e5ce5b57e241393241768a8f56c600
Jeet <jeetnath2110@gmail.com>
2025-02-18 23:16:48 +0530

    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,6 +30,39 @@
                 mousewheel: true,
                 keyboard: true,
             });
+            $('.showcase-products').slick({
+                dots: false,
+                infinite: true,
+                speed: 300,
+                arrows: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                responsive: [{
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: false
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
+            $('.showcase-products .slick-prev').hide();
         });
     </script>
     @endsection
@@ -187,7 +220,7 @@
                                 </div>
                             </div>
                             <div class="col-md-9">
-                                <div class="row">
+                                <div class="row showcase-products">
                                     <div class="col p-2">
                                         <div class="card border-0">
                                             <div class="card-body">
