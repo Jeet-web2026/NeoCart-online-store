a3da9d47b6312ceb5f897538256013707104fe28
Jeet <jeetnath2110@gmail.com>
2025-03-02 18:56:04 +0530

    j

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,12 +1,22 @@
 <div class="col pe-2">
-    <div class="card border-0 shadow-sm no-radius" style="background-color: #F3F3F5;">
-        <div class="d-flex justify-content-center align-items-center">
-            <img src="https://img.freepik.com/free-psd/isolated-modern-laptop_23-2150629074.jpg?uid=R126305893&ga=GA1.1.1378415623.1732413357&semt=ais_hybrid" class="card-img-top no-radius" alt="...">
+    <a href="javascript:void(0)">
+        <div class="card border-0 shadow-sm no-radius" style="background-color: #F3F3F5;">
+            <div class="d-flex justify-content-center align-items-center px-5">
+                <img src="https://img.freepik.com/free-psd/isolated-modern-laptop_23-2150629074.jpg?uid=R126305893&ga=GA1.1.1378415623.1732413357&semt=ais_hybrid" class="card-img-top no-radius" alt="...">
+            </div>
+            <div class="card-body">
+                <p class="text-capitalize fze-1 text-secondary mb-2">company name</p>
+                <h5 class="card-title fze-2 texe-capitalize tetx-black font-500">Card title</h5>
+                <div class="d-flex justify-content-start align-items-center mb-2">
+                    <i class="bi bi-star-fill fze text-warning"></i>
+                    <i class="bi bi-star-fill fze text-warning"></i>
+                    <i class="bi bi-star-fill fze text-warning"></i>
+                    <i class="bi bi-star-fill fze text-warning"></i>
+                    <i class="bi bi-star-fill fze text-warning"></i>
+                    <span class="ms-2 text-secondary">21 comments</span>
+                </div>
+                <h4 class="fze-2 text-capitalize mb-2 fw-bold">₹279</h4>
+            </div>
         </div>
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
+    </a>
 </div>
