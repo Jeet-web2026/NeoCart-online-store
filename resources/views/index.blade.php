8d6744b6bf95ee35896951cb69d1bd6e799d75de
Jeet <jeetnath2110@gmail.com>
2025-03-09 10:58:26 +0530

    j

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -692,7 +692,15 @@
                                         <div class="col pe-2">
                                             <div class="card border-0 bg-transparent">
                                                 <div class="card-body ps-0">
-                                                    This is some text within a card body.
+                                                    <div class="row align-items-center">
+                                                        <div class="col-md-4">
+                                                            <img src="https://img.freepik.com/free-photo/workplace-with-keyboard-near-monitor-computer-mouse-headphones_23-2148040487.jpg?uid=R126305893&ga=GA1.1.1378415623.1732413357&semt=ais_hybrid" alt="computer-image" class="divided-img rounded-2">
+                                                        </div>
+                                                        <div class="col-md-8 ps-3">
+                                                            <h5 class="fze-1 font-500 text-black mb-2 text-capitalize">top laptops</h5>
+                                                            <p class="fze text-capitalize">with 20% discount</p>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -706,9 +714,50 @@
                     </div>
                 </div>
                 <div class="col-md-6 ps-2">
-                    <div class="card border-0 shadow-sm" style="background-color: #F3F3F5;">
-                        <div class="card-body">
-                            This is some text within a card body.
+                <div class="card border-0 shadow-sm" style="background-color: #F3F3F5;">
+                        <div class="card-body p-0">
+                            <div class="row">
+                                <div class="col-md-7 p-3">
+                                    <h3 class="text-capitalize text-black fze-2">comfy styles for your office</h3>
+                                    <p class="my-2 fze-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, minus dicta alias quos cumque aspernatur!</p>
+                                    <a href="javascript:void(0)" class="text-dark text-capitalize">see more<i class="bi bi-arrow-right ms-2"></i></a>
+                                    <div class="row showcase-products-female mt-2">
+                                        <div class="col pe-2">
+                                            <div class="card border-0 bg-transparent">
+                                                <div class="card-body ps-0">
+                                                    <div class="row align-items-center">
+                                                        <div class="col-md-4">
+                                                            <img src="https://img.freepik.com/free-photo/workplace-with-keyboard-near-monitor-computer-mouse-headphones_23-2148040487.jpg?uid=R126305893&ga=GA1.1.1378415623.1732413357&semt=ais_hybrid" alt="computer-image" class="divided-img rounded-2">
+                                                        </div>
+                                                        <div class="col-md-8 ps-3">
+                                                            <h5 class="fze-1 font-500 text-black mb-2 text-capitalize">top laptops</h5>
+                                                            <p class="fze text-capitalize">with 20% discount</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col pe-2">
+                                            <div class="card border-0 bg-transparent">
+                                                <div class="card-body ps-0">
+                                                    <div class="row align-items-center">
+                                                        <div class="col-md-4">
+                                                            <img src="https://img.freepik.com/free-photo/workplace-with-keyboard-near-monitor-computer-mouse-headphones_23-2148040487.jpg?uid=R126305893&ga=GA1.1.1378415623.1732413357&semt=ais_hybrid" alt="computer-image" class="divided-img rounded-2">
+                                                        </div>
+                                                        <div class="col-md-8 ps-3">
+                                                            <h5 class="fze-1 font-500 text-black mb-2 text-capitalize">top laptops</h5>
+                                                            <p class="fze text-capitalize">with 20% discount</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-5 position-relative">
+                                    <img src="https://img.freepik.com/free-photo/young-blackhaired-man-demonstrating-something-bright-laptop_231208-2322.jpg?uid=R126305893&ga=GA1.1.1378415623.1732413357&semt=ais_hybrid" alt="product-img" class="product-img rounded-end-2">
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
