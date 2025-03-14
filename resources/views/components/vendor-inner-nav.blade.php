4cdf42be24eb287ceab9ae8d5b5c2445b5bfa3b8
Jeet <jeetnath2110@gmail.com>
2025-03-14 18:43:39 +0530

    j

--- resources/views/components/vendor-inner-nav.blade.php
+++ resources/views/components/vendor-inner-nav.blade.php
@@ -1,6 +1,6 @@
 @props(['links' => []])
 
-<nav class="navbar navbar-expand-lg bg-dark position-absolute w-100">
+<nav class="navbar navbar-expand-lg bg-dark position-absolute w-100 shadow-sm">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
