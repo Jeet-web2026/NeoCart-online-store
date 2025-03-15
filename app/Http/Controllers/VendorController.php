211d3cd8133d3d316087e177871b72c81e764201
Jeet <jeetnath2110@gmail.com>
2025-03-16 00:48:56 +0530

    j

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -14,7 +14,19 @@ class VendorController extends Controller
     public function AddVendorProducts(Request $request)
     {
         $getData = $request->validate([
-            
+            'product-image' => 'required|mimes:png,jpg,jpeg|max:300',
+            'product-name' => 'required|min:2',
+            'product-price' => 'required|numeric',
+            'product-discount' => 'required|numeric',
+            'product-company' => 'required|min:2',
+            'available-product' => 'required|numeric',
+            'product-description' => 'required|min:5'
+        ],[
+            'product-image.required' => 'Image file is required!',
+            'product-image.mimes' => 'Image file should be must PNG, JPG or JPEG file.',
+            'product-image.max' => 'Image file is must be under 295KB.',
+            'product-name.required' => 'Name is required!',
+            'product-name.min' => 'Name is must be 2 characters long!',
         ]);
     }
 }
