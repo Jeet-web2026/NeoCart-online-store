619c2829e091ee34acf7f25972574ff5e7df75f8
Jeet <jeetnath2110@gmail.com>
2025-03-16 17:55:56 +0530

    j

--- app/Models/VendorLogin.php
+++ app/Models/VendorLogin.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class VendorLogin extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    public $table = 'vendor_logins';
+
+    protected $fillable= [
+        'vendor_login_id',
+        'vendor_login_password'
+    ];
+
+    protected function casts()
+    {
+        return [
+            'vendor_login_password' => 'hashed'
+        ];
+    }
 }
