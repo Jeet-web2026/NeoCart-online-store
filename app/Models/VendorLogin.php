c3bc8c76d589b9ba2c57c6fe2154b2ba054d65d9
Jeet <jeetnath2110@gmail.com>
2025-03-16 19:19:18 +0530

    j

--- app/Models/VendorLogin.php
+++ app/Models/VendorLogin.php
@@ -3,16 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class VendorLogin extends Model
+class VendorLogin extends Authenticatable
 {
     use HasFactory;
-    public $timestamps = false;
-    public $table = 'vendor_logins';
 
-    protected $fillable= [
+    public $timestamps = false;
+    protected $table = 'vendor_logins';
+
+    protected $fillable = [
         'vendor_login_id',
         'vendor_login_password'
     ];
+
+    protected $hidden = [
+        'vendor_login_password',
+    ];
+
+    public function getAuthPassword()
+    {
+        return $this->vendor_login_password;
+    }
 }
