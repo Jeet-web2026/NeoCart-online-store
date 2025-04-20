b6576f6254f21ff732256fc2a9b8941513933f92
Jeet <jeetnath2110@gmail.com>
2025-04-20 13:06:54 +0530

    buttn modification

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,13 @@
             <div class="col-md-6">
                 <div class="card h-100 w-100 border-0 px-5 bg-transparent">
                     <div class="card-body d-flex justify-content-center align-items-center">
-                        <h1 class="text-capitalize fw-bold text-light lh-base">we take care of your IT solutions & provide best services</h1>
+                        <div class="mt-5">
+                            <h1 class="text-capitalize fw-bold text-light lh-sm">we take care of your IT solutions & provide best services</h1>
+                            <div class="d-flex justify-content-start align-items-center mt-5">
+                                <a href="javascript:void(0)" class="btn shadow-sm border-0 text-light text-capitalize contact-now px-4 py-2 me-3">get contact now<i class="bi bi-phone-flip ms-2"></i></a>
+                                <a href="javascript:void(0)" class="text-capitalize text-light text-decoration-none view-services border rounded px-4 py-2">view services<i class="bi bi-box-arrow-in-up-right ms-2"></i></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -28,6 +34,14 @@
         #main-content h1 {
             font-size: 3rem;
             text-shadow: 2px 2px #323232;
+        }
+
+        #main-content .contact-now {
+            background-color: #5404f5;
+        }
+
+        #main-content .view-services {
+            border-color: #5404f5 !important;
         }
     </style>
     @endsection
