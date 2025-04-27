03440783ae77d75603c2058b193cdadadc0c8b6b
Jeet <jeetnath2110@gmail.com>
2025-04-27 21:37:04 +0530

    Figtree font set

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -1,4 +1,4 @@
-@props(['subtitle' => '', 'metaname' => '', 'metaDescription' => '', 'bootstrap' => '', 'jquery' => '', 'poppins' => ''])
+@props(['subtitle' => '', 'metaname' => '', 'metaDescription' => '', 'bootstrap' => '', 'jquery' => '', 'poppins' => '', 'figtree' => ''])
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 
@@ -19,6 +19,14 @@
     @endif
     {{--gogle poppins font--}}
 
+    {{--google figtree font--}}
+    @if($figtree)
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    @endif
+    {{--google figtree font--}}
+
     {{--booststrap css cdn--}}
     @if($bootstrap)
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
@@ -30,6 +38,15 @@
     <style>
         * {
             font-family: "Poppins", sans-serif;
+            font-style: normal;
+        }
+    </style>
+    @endif
+    @if($figtree)
+    <style>
+        * {
+            font-family: "Figtree", sans-serif;
+            font-optical-sizing: auto;
             font-style: normal;
         }
     </style>
