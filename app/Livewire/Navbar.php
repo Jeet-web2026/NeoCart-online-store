58359d608fffde989d8cdb45fa3ed863ea6f70a8
Jeet <jeetnath2110@gmail.com>
2025-04-27 21:29:05 +0530

    Navbar Start

--- app/Livewire/Navbar.php
+++ app/Livewire/Navbar.php
@@ -6,11 +6,6 @@ use Livewire\Component;
 
 class Navbar extends Component
 {
-    public $click = 1;
-    public function handleClick() 
-    {
-        $this->click++;
-    }
     public function render()
     {
         return view('livewire.navbar');
